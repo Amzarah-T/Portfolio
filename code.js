db704d08af31f354d39b7f243f5938adfc8e7d87
Amzarah-T <amzarahtstd@gmail.com>
2023-03-19 21:23:51 +0530

    modified

--- code.js
+++ code.js
@@ -12,3 +12,12 @@ var tablinks = document.getElementsByClassName("tab-links");
         document.getElementById(tabname).classList.add("active-tab");
       }
        
+      var sidemenu = document.getElementById("sidemenu");
+
+      function openmenu() {
+        sidemenu.style.right = "0";
+      }
+
+      function closemenu() {
+        sidemenu.style.right = "-200px";
+      }
